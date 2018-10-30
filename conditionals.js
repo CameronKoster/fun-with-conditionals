529b67b167e287fde0ebdec3e47b4d77856ef0fb
Cameron Koster <kostercam37@gmail.com>
2018-10-30 14:29:38 -0600

    3 problems answered

---
+++ conditionals.js
@@ -0,0 +1,47 @@
+
+//1. Write a function that takes 2 parameters and displays the larger// function dispNum(num1, num2) {
+//   if (num1 > num2) {
+//     return (num1)
+//   }
+//   else if (num1 < num2) {
+//     return (num2)
+//   } else {
+//     return ("equal")
+//   }
+// }
+// console.log(dispNum(8, 8))
+
+// //2. Write a function that will take in a number (1-100) return the letter grade for the student score
+// function grade(num) {
+//   if (num > 90) {
+//     return ("A")
+//   }
+//   else if (num > 80 || num < 89) {
+//     return ("B")
+//   }
+//   else if (num > 70 || num < 79) {
+//     return ("C")
+//   }
+//   else if (num > 60 || num < 69) {
+//     return ("D")
+//   }
+//   else { }
+// }
+// console.log(grade(83))
+
+
+//3. Write a function that will take in a number and return 'fever' if it indicates a fever 
+// (over 98.6) and additionally if the person should go to the hospital(over 103) 'fever go to hospital'
+// (hint: try this with string concatenation), if it is under return 'no fever'
+// function temp(num) {
+//   if (num >= 103) {
+//     return ("fever go to hospital")
+//   }
+//   else if (num >= 98.6 || num < 102.99) {
+//     return ("fever")
+//   }
+//   else if (num < 98.6) {
+//     return ("no fever")
+//   }
+// }
+// console.log(temp(101))
